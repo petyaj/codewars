1d0aeaea290ebe9856555517875262d2e99ad136
Rutsevich <rutsevich@gmail.com>
2023-11-26 19:13:13 +0100

    add Methods of string object solution

---
+++ 8kyu/methods-of-string-object.js
@@ -0,0 +1,20 @@
+// ❓ DESCRIPTION:
+//      Implement a function which accepts 2 arguments: string and separator.
+//      The expected algorithm: split the string into words by spaces, split each word into separate characters and join
+//      them back with the specified separator, join all the resulting "words" back into a sentence with spaces.
+//
+//      For example:
+//          splitAndMerge("My name is John", " ")  ==  "M y n a m e i s J o h n"
+//          splitAndMerge("My name is John", "-")  ==  "M-y n-a-m-e i-s J-o-h-n"
+//          splitAndMerge("Hello World!", ".")     ==  "H.e.l.l.o W.o.r.l.d.!"
+//          splitAndMerge("Hello World!", ",")     ==  "H,e,l,l,o W,o,r,l,d,!"
+
+// ✅ SOLUTION:
+function splitAndMerge(string, separator) {
+    return string.split(" ").map(w => w.split("").join(separator)).join(" ");
+}
+
+console.log("solution: " + splitAndMerge("My name is John", " "));
+console.log("solution: " + splitAndMerge("My name is John", "-"));
+console.log("solution: " + splitAndMerge("Hello World!", "."));
+console.log("solution: " + splitAndMerge("Hello World!", ","));
